5dadd5a5987e8fe18863d0b23b502d117e45b2a1
Jeremy Cheron <jcheron@student.42lehavre.fr>
2025-01-28 12:47:31 +0100

    + | cd

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,21 +6,31 @@
 /*   By: jcheron <jcheron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 12:03:10 by jcheron           #+#    #+#             */
-/*   Updated: 2025/01/28 12:16:41 by jcheron          ###   ########.fr       */
+/*   Updated: 2025/01/28 12:45:47 by jcheron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 #include "../../third_party/42_libft_full/libft.h"
 
-int	ft_echo(char **args)
+/**
+ * @brief		Mimics the behavior of the Unix 'echo' command.
+ *
+ * @details 	This function prints the provided arguments to the standard
+ * 				outuput. If the first argument is "-n", it suppresses the
+ * 				new line at the end of the output. Each argument is seperated
+ * 				by a single space when printed.
+ */
+void	ft_echo(
+			char **args
+			)
 {
 	int		i;
 	int		n_flag;
 
-	i = 1;
+	i = 0;
 	n_flag = 0;
-	if (args[1] && !ft_strcmp(args[1], "-n"))
+	if (args[0] && !ft_strcmp(args[0], "-n"))
 	{
 		n_flag = 1;
 		i++;
