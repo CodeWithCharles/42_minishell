27c4edf20f5c960a143c065d18cdf9d626e0da2f
Jeremy Cheron <jcheron@student.42lehavre.fr>
2025-01-31 09:59:07 +0100

    ~ | update structs for parsing

---
+++ src/builtins/export.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcheron <jcheron@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/31 08:56:23 by jcheron           #+#    #+#             */
+/*   Updated: 2025/01/31 09:08:55 by jcheron          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	ft_export(
+	t_minishell_ctx *ctx,
+	char **args
+)
+{
+	char	*name;
+	char	*value;
+	t_list	*node;
+
+	(void)ctx;
+	if (!args || !ft_strchr(*args, '='))
+	{
+		fd_printf(STDERR_FILENO, "export : invalid format\n");
+		return ;
+	}
+	// skip $ && return NAME=VALUE
+}
--- src/execution/execute_builtin.c
+++ src/execution/execute_builtin.c
@@ -6,7 +6,7 @@
 /*   By: jcheron <jcheron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 12:21:38 by onkeltag          #+#    #+#             */
-/*   Updated: 2025/01/30 17:32:55 by jcheron          ###   ########.fr       */
+/*   Updated: 2025/01/31 09:03:59 by jcheron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ int	execute_builtin(
 		ft_env(ctx);
 	else if (ft_strcmp(args[0], "exit") == 0)
 		ft_exit(ctx, args + 1);
-	// else if (ft_strcmp(args[0], "export") == 0)
-	// 	ft_export(ctx, args + 1);
+	else if (ft_strcmp(args[0], "export") == 0)
+		ft_export(ctx, args + 1);
 	else if (ft_strcmp(args[0], "pwd") == 0)
 		ft_pwd(ctx);
 	// else if (ft_strcmp(args[0], "unset") == 0)
