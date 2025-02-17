603537d40f946214ac56256802289cbfa3ac3cf5
Charles POULAIN <charles.probe@gmail.com>
2025-02-17 17:16:01 +0100

    ~ | Ez here_doc leak fix

--- src/execution/here_doc.c
+++ src/execution/here_doc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   here_doc.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cpoulain <cpoulain@student.42lehavre.fr>   +#+  +:+       +#+        */
+/*   By: cpoulain <cpoulain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 19:27:30 by cpoulain          #+#    #+#             */
-/*   Updated: 2025/02/12 13:31:36 by cpoulain         ###   ########.fr       */
+/*   Updated: 2025/02/17 17:15:36 by cpoulain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,10 +39,9 @@ static int	write_here_doc_to_file(
 	doc_fd = open((*redir)->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (doc_fd == INVALID_FD)
 		return (print_gen_error(ctx, ERR_COULD_NOT_OPEN_HD), RET_ERR);
-	line = NULL;
-	while (1)
+	line = readline(HERE_DOC_PREFIX);
+	while (line)
 	{
-		line = readline(HERE_DOC_PREFIX);
 		if (ft_strcmp(line, delimiter) == 0)
 		{
 			free(line);
@@ -51,6 +50,7 @@ static int	write_here_doc_to_file(
 		write(doc_fd, line, ft_strlen(line));
 		write(doc_fd, "\n", 1);
 		free(line);
+		line = readline(HERE_DOC_PREFIX);
 	}
 	close(doc_fd);
 	return (RET_OK);
